21a6617badaaa5c0d618e5e50134a45be71ea01f
Tran Duc Khang <79127891+KhangTran2005@users.noreply.github.com>
2022-03-21 14:52:40 +0800

    clustering backend done

--- clustering.py
+++ clustering.py
@@ -1,11 +1,61 @@
+#---- Must Install -----
+# pys2, networkx, infomap, requests
+# Query format: python -m clustering "[User query here]" depth (put 1 for testing)
+#---- End Must Install ----
+
 import sys
+import s2
+import networkx as nx
+from infomap import Infomap
+import requests
 
+def get_graph(s, G, depth=5, marked = {}):
+  # Mark current node as visited
+  marked[s] = True
 
-def get_graph(query):
-    return {'a': ['b', 'c', 'd'],
-            'b': ['a', 'c']}
+  # Query API to get neigbours
+  paper = s2.api.get_paper(s)
+  neighbours = []
+  for i in paper.citations + paper.references:
+    neighbours.append(i.paperId)
+  
+  G[s] = neighbours
+
+  if (depth == 0):
+    return
+
+  for v in neighbours:
+    if v in marked: continue
+    get_graph(v, G, depth - 1, marked)
+
+def get_clustered_graph(G, depth_level=10):
+  im = Infomap(silent=True)
+  mapping = im.add_networkx_graph(G)
+  im.run()
+  clustering = im.get_modules(depth_level=depth_level)
+  id = mapping.values()
+  clust = clustering.values()
+  graph_data = {}
+  graph_data["nodes"] = [{"id": a, "group": b} for a, b in zip(id, clust)]
+  graph_data["links"] = [{"source": a, "target": b, "value": 1} for a, b in G.edges]
+  return graph_data
 
 if __name__ == '__main__':
+    #Getting input
     arg = sys.argv
-    if arg[1] == 'get_graph':
-        print(get_graph(arg[2]))
+    user_in = arg[1]
+    query = '+'.join(user_in.lower().split())
+    depth = int(arg[2])
+    if len(arg) == 4:
+        depth_level = arg[3]
+    else:
+        depth_level = 10
+
+    #Run the codes
+    response = requests.get(f'https://api.semanticscholar.org/graph/v1/paper/search?query={query}&limit=1')
+    root = response.json()['data'][0]['paperId']
+
+    graph_data = {}
+    get_graph(root, graph_data, depth)
+    G = nx.Graph(graph_data)
+    print(get_clusters(G, depth_level))
